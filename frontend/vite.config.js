d91cd37bafe31fa7b83999a9a8432d9946738945
phewee12 <ganji.1026@gmail.com>
2026-04-06 23:34:07 -0400

    feature/resume-feedback
    -added feedback feature, included sliders and AI feedback and summaries.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  server: {
+    proxy: {
+      // This redirects all frontend '/api' calls to your backend port
+      '/api': {
+        target: 'http://localhost:4000', 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
